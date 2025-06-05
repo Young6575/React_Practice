e8aae24b84df39591d076d2027f265c70d4cf0b5
Young6575 <wnsdud11510@gmail.com>
2025-06-05 13:11:26 +0900

    React 기본

---
+++ src/01/GroupText.jsx
@@ -0,0 +1,13 @@
+function GroupText () {
+    return(
+        <div className="text-2xl bg-emerald-500 text-white mb-2 p-2">
+            K-digital 2기
+        </div>
+        
+
+
+        
+    )
+}
+
+export default GroupText
